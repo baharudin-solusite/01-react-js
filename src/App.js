05dabfd3e5d68b4a2cc27142ae240fea6300d938
Baharudin <baharudinfahrul85@gmail.com>
2022-07-02 01:44:40 +0700

    review materi react hari pertama

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+// import data/halaman
 import SelamatDatang from './components/SelamatDatang';
 import './App.css';
 import SelamatMalam from './components/SelamayMalam';
+// Import gambar
 import BurungHatu from "./img/burung-hantu.jpg"
 function App() {
   // tempat membuat Variabel sebelum return
@@ -10,20 +12,30 @@ function App() {
   //   console.log("ayam")
   // }
   const handleClick = () => {
-    console.log('Burung Hantu')
+    console.log("burung hantu")
   }
+
+  // conditional
+  const isLogin = true
+
+  // map
+  const data = [
+   {
+    orang:"aurel",
+   }, {orang : "anton",
+
+   },{orang : "budi" }
+  ]
 
 
   return (
     // JSX
+    // react membutuhkan pembunggkus untuk parentnya
     <div className="App">
-      <div>
       <header className="App-header">
-        
         <SelamatDatang/>  
-        <button onClick={() => console.log("cobaan ke 2")}>Inlane</button>
-        <button onClick={() => console.log("inlane Button")}>Inlane Button</button>     
-        <button onClick={handleClick}>Tes</button>
+      
+        {/* url gambar src */}
         <img width={400} src="https://rimbakita.com/wp-content/uploads/2019/01/burung-hantu.jpg" alt='Burung Hantu'/>
         <SelamatMalam/>
         <img width={400} src={BurungHatu} alt='Burung Hantu'/>
@@ -38,10 +50,25 @@ function App() {
         <p> {"surabaya".toUpperCase ()} </p> 
 
 
+          {/* Event */}
+        {/* Pembuatan Button secara inlane */}
+        <button onClick={() => console.log("inlane Button")}>Inlane Button</button>     
+        {/* pembuattan button secara internal */}
+        <button onClick={handleClick}>Tes</button>
+        {/* checkbox */}
+        <input type={"checkbox"} onChange = {() => console.log("ayam")} />
+        {/* input form */}
+        <input type={"text"} onChange = {(event) => console.log(event.target.value)}/>
 
-       
-      </header>
-      </div>
+
+        {/* conditional */}
+        {/* codisional yang bisa dipakai pada React Js Ternari operator */}
+        {isLogin ? <p>sudah login</p> : <p>belum login</p> }
+        {/* maping menampilkan Array of object */}
+        {data.map ((item,index) => ( 
+          <h1 key={index}>{item.orang}</h1>
+        ))}
+      </header>   
     </div>
   );
 }
